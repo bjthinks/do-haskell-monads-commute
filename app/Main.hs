60ab68b3e313dd8beb79e997f0f2d0a273543b3a
bjthinks <bjthinks@gmail.com>
2025-12-23 15:29:23 -0800

    Add first three function templates

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,19 @@
 module Main where
 
+readerState :: IO ()
+readerState = do
+  return ()
+
+readerWriter :: IO ()
+readerWriter = do
+  return ()
+
+stateWriter :: IO ()
+stateWriter = do
+  return ()
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  readerState
+  readerWriter
+  stateWriter
