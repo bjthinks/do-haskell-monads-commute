803cc67191b9b4934f148d505cf9406dcbe3f6ef
Brian Johnson <bjthinks@gmail.com>
2025-12-25 04:01:37 -0800

    Combine Reader & Writer, and State & Writer

--- app/Main.hs
+++ app/Main.hs
@@ -13,7 +13,7 @@ line str = tell str >> nl
 readerState :: Writer String ()
 readerState = do
   line "Composing Reader and State monads."
-  line "Program: do { x <- get ; y <- ask ; put (x+y) }"
+  line "do { x <- get ; y <- ask ; put (x+y) }"
   let doA = do { x <- get ; y <- ask ; put (x+y) } :: ReaderT Int (State Int) ()
       doB = do { x <- get ; y <- ask ; put (x+y) } :: StateT Int (Reader Int) ()
   line "Running with Reader data 2, State data 3"
@@ -21,22 +21,35 @@ readerState = do
   let resultB = runReader (runStateT doB 3) 2
   line $ "Monad = ReaderT Int (State Int), result: " ++ show resultA
   line $ "Monad = StateT Int (Reader Int), result: " ++ show resultB
+  line "Conclusion: Reader and State commute."
   return ()
 
 readerWriter :: Writer String ()
 readerWriter = do
   line "Composing Reader and Writer monads."
-  line "Program: do { x <- ask ; tell (show x) }"
+  line "do { x <- ask ; tell (show x) }"
   let doA = do { x <- ask ; tell (show x) } :: ReaderT Int (Writer String) ()
       doB = do { x <- ask ; tell (show x) } :: WriterT String (Reader Int) ()
+  line "Running with Reader data 2"
+  let resultA = runWriter (runReaderT doA 2)
+  let resultB = runReader (runWriterT doB) 2
+  line $ "Monad = ReaderT Int (Writer String), result: " ++ show resultA
+  line $ "Monad = WriterT String (Reader Int), result: " ++ show resultB
+  line "Conclusion: Reader and Writer commute."
   return ()
 
 stateWriter :: Writer String ()
 stateWriter = do
   line "Composing State and Writer monads."
-  line "Program: do { x <- get ; tell (show x) ; put (x+1) }"
+  line "do { x <- get ; tell (show x) ; put (x+1) }"
   let doA = do { x <- get ; tell (show x) ; put (x+1) } :: StateT Int (Writer String) ()
       doB = do { x <- get ; tell (show x) ; put (x+1) } :: WriterT String (State Int) ()
+  line "Running with State data 3"
+  let resultA = runWriter (runStateT doA 3)
+  let resultB = runState (runWriterT doB) 3
+  line $ "Monad = StateT Int (Writer String), result: " ++ show resultA
+  line $ "Monad = WriterT String (State Int), result: " ++ show resultB
+  line "Conclusion: State and Writer essentially commute."
   return ()
 
 allPairs :: Writer String ()
